8afaf892ab62ca6f5ddea0af96371f20781d94d3
ELMKV <ilya.makoveev021199@gmail.com>
2020-10-06 19:40:47 +0500

    fix

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,46 +1,46 @@
 package com.example.weather;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Looper;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static android.app.AlertDialog.Builder;
+
 public class MainActivity extends AppCompatActivity {
-    private FusedLocationProviderClient fusedLocationClient;
+    private static final int REQUEST_LOCATION = 1;
+    LocationManager locationManager;
+    String latitude, longitude, iconCode;
+
+
+    Button btnGetLocation;
     double lat;
     double lon;
     FusedLocationProviderClient mFusedLocationClient;
 
-    // Initializing other items
-    // from layout file
-    TextView nameTextView, tempMinTextView, tempMaxTextView, feelsLikeTextView, cloudsTextView;
+    TextView nameTextView, tempMinTextView, tempMaxTextView, feelsLikeTextView, cloudsTextView, codeIcon;
+    ImageView imageViewIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,12 +52,71 @@ public class MainActivity extends AppCompatActivity {
         tempMinTextView = findViewById(R.id.tempMin);
         feelsLikeTextView = findViewById(R.id.feelsLike);
         cloudsTextView = findViewById(R.id.cloudsTextView);
+        btnGetLocation = findViewById(R.id.btnGetLocation);
+        codeIcon = findViewById(R.id.codeIcon);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
 
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            OnGPS();
+
+        } else {
+            getLocation();
+            loadInfo();
+        }
+
+        btnGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
+    private void OnGPS() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
 
+        final Builder builder = new Builder(this);
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+}
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (locationGPS != null) {
+                lat = locationGPS.getLatitude();
+                lon = locationGPS.getLongitude();
+
+                Log.d("loc", String.valueOf(lat));
+                Log.d("loc", String.valueOf(lon));
+
+            } else {
+                Toast.makeText(this, "Unable to find location.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     private void loadInfo() {
         NetworkService
@@ -76,6 +135,8 @@ public class MainActivity extends AppCompatActivity {
                         feelsLikeTextView.setText(info.getList().get(cLoc).getMain().getFeelsLike().toString());
                         feelsLikeTextView.setText(info.getList().get(cLoc).getMain().getFeelsLike().toString());
                         cloudsTextView.setText(info.getList().get(cLoc).getWeather().get(cLoc).getDescription());
+                        codeIcon.setText(info.getList().get(cLoc).getWeather().get(cLoc).getIcon());
+
 
 
                     }
@@ -87,7 +148,5 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    public void reload(View view) {
-        loadInfo();
-    }
+
 }
