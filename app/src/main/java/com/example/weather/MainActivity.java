afec56453e1042b6e8648dcae8a79377d320788f
ELMKV <ilya.makoveev021199@gmail.com>
2020-10-06 16:25:37 +0500

    delete last code

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -41,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
     // Initializing other items
     // from layout file
     TextView nameTextView, tempMinTextView, tempMaxTextView, feelsLikeTextView, cloudsTextView;
-    int PERMISSION_ID = 44;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,151 +53,11 @@ public class MainActivity extends AppCompatActivity {
         feelsLikeTextView = findViewById(R.id.feelsLike);
         cloudsTextView = findViewById(R.id.cloudsTextView);
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        // method to get the location
-        getLastLocation();
+
 
     }
 
-    @SuppressLint("MissingPermission")
-    private void getLastLocation() {
-        // check if permissions are given
-        if (checkPermissions()) {
 
-            // check if location is enabled
-            if (isLocationEnabled()) {
-
-                // getting last
-                // location from
-                // FusedLocationClient
-                // object
-                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-
-                    @Override
-                    public void onComplete(
-                            @NonNull Task<Location> task) {
-                        Location location = task.getResult();
-                        if (location == null) {
-                            requestNewLocationData();
-                        } else {
-                            lat = location.getLatitude();
-                            lon = location.getLongitude();
-                            Log.d("location", String.valueOf(lat));
-                            Log.d("location", String.valueOf(lon));
-
-                        }
-                    }
-                });
-            } else {
-                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        } else {
-            // if permissions aren't available,
-            // request for permissions
-            requestPermissions();
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void requestNewLocationData() {
-
-        // Initializing LocationRequest
-        // object with appropriate methods
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(5);
-        mLocationRequest.setFastestInterval(0);
-        mLocationRequest.setNumUpdates(1);
-
-        // setting LocationRequest
-        // on FusedLocationClient
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
-    }
-
-    private LocationCallback
-            mLocationCallback = new LocationCallback() {
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            Location mLastLocation = locationResult.getLastLocation();
-
-        }
-    };
-
-    // method to check for permissions
-    private boolean checkPermissions() {
-        return ActivityCompat
-                .checkSelfPermission(
-                        this,
-                        Manifest.permission
-                                .ACCESS_COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED
-
-                && ActivityCompat
-                .checkSelfPermission(
-                        this,
-                        Manifest.permission
-                                .ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED;
-
-        // If we want background location
-        // on Android 10.0 and higher,
-        // use:
-        /* ActivityCompat
-                .checkSelfPermission(
-                    this,
-                    Manifest.permission
-                        .ACCESS_BACKGROUND_LOCATION)
-            == PackageManager.PERMISSION_GRANTED
-        */
-    }
-
-    // method to requestfor permissions
-    private void requestPermissions() {
-        ActivityCompat.requestPermissions(
-                this,
-                new String[]{
-                        Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
-    }
-
-    // method to check
-    // if location is enabled
-    private boolean isLocationEnabled() {
-        LocationManager
-                locationManager = (LocationManager) getSystemService(
-                Context.LOCATION_SERVICE);
-
-        return locationManager
-                .isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
-    // If everything is alright then
-    @Override
-    public void
-    onRequestPermissionsResult(
-            int requestCode,
-            @NonNull String[] permissions,
-            @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_ID) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastLocation();
-                loadInfo();
-            }
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (checkPermissions()) {
-            getLastLocation();
-        }
-    }
 
     private void loadInfo() {
         NetworkService
@@ -229,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void reload(View view) {
-        getLastLocation();
         loadInfo();
     }
 }
