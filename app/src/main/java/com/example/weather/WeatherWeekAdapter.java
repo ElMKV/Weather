c38a0f0a636f9f0f145aab6114b4885e1c537c3f
ELMKV <ilya.makoveev021199@gmail.com>
2020-10-09 02:04:45 +0500

    Добавил адаптер для отображения недельной погоды

--- app/src/main/java/com/example/weather/WeatherWeekAdapter.java
+++ app/src/main/java/com/example/weather/WeatherWeekAdapter.java
@@ -1,21 +1,26 @@
 package com.example.weather;
 
 
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 
 public class WeatherWeekAdapter extends RecyclerView.Adapter<WeatherWeekAdapter.ViewHolder> {
     Context context;
+    String codeWeatherWeek;
     List<Daily> list;
     public WeatherWeekAdapter(Context context, List<Daily> list) {
         this.context = context;
@@ -31,8 +36,24 @@ public class WeatherWeekAdapter extends RecyclerView.Adapter<WeatherWeekAdapter.
     }
     @Override
     public void onBindViewHolder(@NonNull WeatherWeekAdapter.ViewHolder holder, int position) {
-        holder.dayWeather.setText(list.get(position).getTemp().getDay() + "");
-        holder.nightWeather.setText(list.get(position).getTemp().getNight() + "");
+        Double day = list.get(position).getTemp().getDay();
+        Double night = list.get(position).getTemp().getNight();
+        day = Math.ceil(day);
+        night = Math.ceil(night);
+        holder.dayWeather.setText(day + "");
+        holder.nightWeather.setText(night + "");
+
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+        Date dateFormat = new java.util.Date(list.get(position).getDt() * 1000);
+        String week = sdf.format(dateFormat);
+        week = week.substring(0, 1).toUpperCase() + week.substring(1);
+        holder.textViewNameWeek.setText(week);
+
+        codeWeatherWeek = list.get(position).getWeather().get(0).getIcon();
+
+        Picasso.with(context)
+                .load("http://openweathermap.org/img/wn/" + codeWeatherWeek + "@2x.png")
+                .into(holder.imageViewWeatherWeek);
 
     }
     @Override
@@ -40,11 +61,16 @@ public class WeatherWeekAdapter extends RecyclerView.Adapter<WeatherWeekAdapter.
         return list.size();
     }
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView dayWeather, nightWeather;
+        TextView dayWeather, nightWeather, textViewNameWeek;
+        ImageView imageViewWeatherWeek;
         public ViewHolder(View itemView) {
             super(itemView);
             dayWeather = (TextView) itemView.findViewById(R.id.textViewDayTemp);
             nightWeather = (TextView) itemView.findViewById(R.id.textViewNightTemp);
+            textViewNameWeek = (TextView) itemView.findViewById(R.id.textViewNameWeek);
+            imageViewWeatherWeek = (ImageView) itemView.findViewById(R.id.imageViewWeatherWeek);
+
+
             itemView.setOnClickListener(this);
 
         }
