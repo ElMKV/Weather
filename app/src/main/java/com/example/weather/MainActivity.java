758fa49d2842804445acd537cb138f31a07d7a49
ELMKV <ilya.makoveev021199@gmail.com>
2020-10-17 18:23:38 +0500

    clean

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,16 +1,7 @@
 package com.example.weather;
 
-import android.Manifest;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -18,7 +9,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -38,18 +28,16 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-import static android.app.AlertDialog.Builder;
-
 public class MainActivity extends AppCompatActivity {
     LineGraphSeries<DataPoint> series;
     private static final int REQUEST_LOCATION = 1;
     String KEY = "14fcc3726972a2ee7e67a8a0a98c87d8";
     String UNITS = "metric";
     String LANG = "ru";
-    LocationManager locationManager;
+
     String codeWeather;
-    Double lat;
-    Double lon;
+    Double lat, lon;
+
     TextView nameTextView, sunSetTextView, sunRiseTextView, feelsLikeTextView, cloudsTextView, textViewPower, textViewHumidity,
             textViewCloudsPer, textViewVisibility, textViewUviIndex, textViewWindSpeed, textViewWindDeg;
     RecyclerView recyclerView;
@@ -64,8 +52,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getView();
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-        checkEneable();
+
     }
 
     private void getView() {
@@ -98,68 +85,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void checkEneable() {
-        Log.d("LOGG", "checkEneable");
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Log.d("LOGG", "if checkEneable");
-            OnGPS();
 
-        } else {
-            getLocation();
-            Log.d("LOGG", "else checkEneable");
-        }
-    }
 
     @Override
     protected void onRestart() {
         Log.d("LOGG", "onResume");
         super.onRestart();
-        checkEneable();
+        
     }
 
-    private void OnGPS() {
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
 
-            final Builder builder = new Builder(this);
-            builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                }
-            }).setNegativeButton("No", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.cancel();
-                }
-            });
-            final AlertDialog alertDialog = builder.create();
-            alertDialog.show();
-        }
 
-    }
-
-    private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-        } else {
-            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (locationGPS != null) {
-                lat = locationGPS.getLatitude();
-                lon = locationGPS.getLongitude();
-                Log.d("loc", String.valueOf(lat));
-                Log.d("loc", String.valueOf(lon));
-
-                loadInfo();
-
-            } else {
-                Toast.makeText(this, "Не удалось найти местоположение", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
 
     private void loadInfo() {
         NetworkService.getInstance()
